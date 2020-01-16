b1e11eef8c673180e28c05065ca05901f5f0bc59
Jeremy Marino <jermarino16@gmail.com>
2020-01-16 14:28:34 -0800

    able to script loading the webpage where the forms are located

---
+++ Forms/fill_out_forms.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+import chromedriver_binary  # Adds chromedriver binary to path
+
+def get_form_page():
+	browser = webdriver.Chrome()
+
+	browser.get("https://ccbf.net/wp-login.php?loggedout=true")#make sure you are logged out of ccbf
+	browser.get("https://ccbf.net/administrative-forms/") #get the forms login page
+
+	#get the password text box
+	password_element = browser.find_element_by_css_selector("#pwbox-1637") #get the password box
+	password_element.click()
+	password_element.send_keys("p@ssw0rd")
+
+	#click enter to enter password
+	submit_element = browser.find_element_by_name("Submit") #get the password box
+	submit_element.click()
