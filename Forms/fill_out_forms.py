b95275ad173d0036947067c932125941f38a1107
Jeremy Marino <jermarino16@gmail.com>
2020-01-16 19:02:04 -0800

    got browser to switch tabs for event request form

--- Forms/fill_out_forms.py
+++ Forms/fill_out_forms.py
@@ -1,8 +1,18 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import chromedriver_binary  # Adds chromedriver binary to path
 
+browser = webdriver.Chrome()
+name, email = "", ""
+
+def get_user_info():
+	global name, email
+
+	name = input("Who's filling out the form? ")
+	email = input("What's the email? ")
+
 def get_form_page():
-	browser = webdriver.Chrome()
+	global browser
 
 	browser.get("https://ccbf.net/wp-login.php?loggedout=true")#make sure you are logged out of ccbf
 	browser.get("https://ccbf.net/administrative-forms/") #get the forms login page
@@ -18,4 +28,26 @@ def get_form_page():
 	# pause()
 	return browser
 
+def submit_event_form():
+	global browser
+	# open the hyperlink
+	event_planning_link = browser.find_element_by_css_selector("body > div.sticky_top > section > div > ol > li:nth-child(1) > a")
+	event_planning_link.click()
+
+	# switch to the tab that opened the form
+	browser.find_element_by_tag_name('body').send_keys(Keys.CONTROL + Keys.TAB)
+	# print(len(browser.window_handles)) #check how many windows we have open, should be 2
+	browser.switch_to.window(browser.window_handles[1])
+
+	print(browser.current_url) #what url are we on
+	# #type in name
+	# name_field = browser.find_element_by_id("nf-field-721")
+	# name_field.click()
+	# # name_field.send_keys(name)
+	# name_field.send_keys("test")
+
+	return browser
+
+# get_user_info()
 browser = get_form_page()
+browser = submit_event_form()
