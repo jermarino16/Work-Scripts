53b5cbbfdb4f7470e6a03464e6c32ceea221954b
Jeremy Marino <jermarino16@gmail.com>
2020-01-17 08:15:58 -0800

    automated filling out page 1

--- Forms/expenditure_request.py
+++ Forms/expenditure_request.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+
 import chromedriver_binary  # Adds chromedriver binary to path
 
 browser = webdriver.Chrome()
@@ -33,22 +34,60 @@ def get_expenditure_form():
 	#click the hyperlink
 	expenditure_hyperlink = browser.find_element_by_css_selector("body > div.sticky_top > section > div > ol > li:nth-child(2) > a")
 	expenditure_hyperlink.click()
+	switch_tabs()
 
 def switch_tabs():
 	# switch to the tab that opened the form
 	browser.find_element_by_tag_name('body').send_keys(Keys.CONTROL + Keys.TAB)
 	# print(len(browser.window_handles)) #check how many windows we have open, should be 2
-	browser.switch_to.window(browser.window_handles[1])
+	browser.switch_to.window(browser.window_handles[-1])
 
-def fill_out_page_1():
+def type_name():
 	name_field = browser.find_element_by_css_selector("#nf-field-740")
 	name_field.click()
 	name_field.send_keys("Jeremy is cool")
 
+def type_phone():
+	phone_field = browser.find_element_by_css_selector("#nf-field-741")
+	phone_field.click()
+	phone_field.send_keys("951-764-2881")
+
+def type_email():
+	email_field = browser.find_element_by_css_selector("#nf-field-742")
+	email_field.click()
+	email_field.send_keys("jeremy@ccbf.net")
+
+def next_page():
+	next_button = browser.find_element_by_class_name("nf-next")
+	next_button.click()
+
+def fill_out_page_1():
+	type_name()
+	type_phone()
+	type_email()
+	next_page()
+	switch_tabs()
+
+def type_description():
+	description_field = browser.find_element_by_css_selector("#nf-field-747")
+	description_field.click()
+	description_field.send_keys("Jeremy So cool")
+
+def select_ministry():
+	ministry_field = browser.find_element_by_css_selector("#nf-field-755")
+	ministry_field.click()
+	ministry_field.send_keys("Junior High")
+	ministry_field.click()	
+	
+def fill_out_page_2():
+	type_description()
+	select_ministry()
+
+
 get_forms_page()
 get_expenditure_form()
-switch_tabs()
 fill_out_page_1()
+fill_out_page_2()
 
 
 
