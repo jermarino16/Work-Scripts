65d2fd3d04aeec89a7d3ec9bb84fb18b00065f52
Jeremy Marino <jermarino16@gmail.com>
2020-01-22 11:26:47 -0800

    refactoring to have a ninja forms class for code reusability, in the process right now

--- Forms/expenditure_request.py
+++ Forms/expenditure_request.py
@@ -1,37 +1,46 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.common.exceptions import NoSuchElementException
+#refactord to ninja forms automator
+# from selenium import webdriver
+# from selenium.webdriver.common.keys import Keys
+# from selenium.common.exceptions import NoSuchElementException
 
-import chromedriver_binary  # Adds chromedriver binary to path
-import functools #for error handling
+# import chromedriver_binary  # Adds chromedriver binary to path
+# import functools #for error handling
 
-from time import sleep #to pause execution
 
-browser = ""
-user_name, email, phone = "", "", ""
+# from time import sleep #to pause execution
+
+
+import ninja_forms_automator
+
+#refactored to ninja forms class
+# browser = ""
+# user_name, email, phone = "", "", ""
+
 purchase_description, purchase_ministry, purchase_category = "", "", ""
 purchase_amount, payment_type, payment_to, notes = "", "", "", ""
 
-def get_default_user_values():
-	global user_name, user_email, user_phone
-	print("Proceeding with default values")
+# refactoring to ninja forms class
+# def get_default_user_values():
+# 	global user_name, user_email, user_phone
+# 	print("Proceeding with default values")
 
-	user_name = "Jeremy Marino"
-	user_email = "Jeremy@ccbf.net"
-	user_phone = "951-764-2881"
+# 	user_name = "Jeremy Marino"
+# 	user_email = "Jeremy@ccbf.net"
+# 	user_phone = "951-764-2881"
 
-def get_user_info():
-	global user_name, user_email, user_phone
+# refactoring to ninja forms class
+# def get_user_info():
+# 	global user_name, user_email, user_phone
 
-	default_values = input("Do you want to use default values? 1 for yes : ")
-	if default_values == "1":
-		get_default_user_values()
-	else: 
-		user_name = input("Who's filling out the form? ")
-		user_email = input("What's the email? ")
-		user_phone = input("What's your phone number? ")
+# 	default_values = input("Do you want to use default values? 1 for yes : ")
+# 	if default_values == "1":
+# 		get_default_user_values()
+# 	else: 
+# 		user_name = input("Who's filling out the form? ")
+# 		user_email = input("What's the email? ")
+# 		user_phone = input("What's your phone number? ")
 
-	get_purchase_info()
+	# get_purchase_info()
 
 def get_purchase_info():
 	global purchase_description, purchase_ministry, purchase_category, purchase_amount, payment_type, payment_to, notes
@@ -57,23 +66,24 @@ def exception(fn):
 
 	return wrapper
 
-@exception
-def get_forms_page():
-	global browser
+# refactored to ninja forms class
+# @exception
+# def get_forms_page():
+# 	global browser
 
-	browser.get("https://ccbf.net/wp-login.php?loggedout=true")#make sure you are logged out of ccbf
-	browser.get("https://ccbf.net/administrative-forms/") #get the forms login page
+# 	browser.get("https://ccbf.net/wp-login.php?loggedout=true")#make sure you are logged out of ccbf
+# 	browser.get("https://ccbf.net/administrative-forms/") #get the forms login page
 
-	#get the password text box
-	password_element = browser.find_element_by_css_selector("#pwbox-1637") #get the password box
-	password_element.click()
-	password_element.send_keys("p@ssw0rd")
+# 	#get the password text box
+# 	password_element = browser.find_element_by_css_selector("#pwbox-1637") #get the password box
+# 	password_element.click()
+# 	password_element.send_keys("p@ssw0rd")
 
-	#click enter to enter password
-	submit_element = browser.find_element_by_name("Submit") #get the password box
-	submit_element.click()
-	# pause()
-	return browser
+# 	#click enter to enter password
+# 	submit_element = browser.find_element_by_name("Submit") #get the password box
+# 	submit_element.click()
+# 	# pause()
+# 	return browser
 
 @exception
 def get_expenditure_form():
@@ -82,51 +92,56 @@ def get_expenditure_form():
 	expenditure_hyperlink.click()
 	switch_tabs()
 
-@exception
-def switch_tabs():
-	# switch to the tab that opened the form
-	browser.find_element_by_tag_name('body').send_keys(Keys.CONTROL + Keys.TAB)
-	# print(len(browser.window_handles)) #check how many windows we have open, should be 2
-	browser.switch_to.window(browser.window_handles[-1])
+# refactored to ninja forms class
+# @exception
+# def switch_tabs():
+# 	# switch to the tab that opened the form
+# 	browser.find_element_by_tag_name('body').send_keys(Keys.CONTROL + Keys.TAB)
+# 	# print(len(browser.window_handles)) #check how many windows we have open, should be 2
+# 	browser.switch_to.window(browser.window_handles[-1])
 
-@exception
-def type_name():
-	# #find by css_selector --- this works for this page
-	# name_field = browser.find_element_by_css_selector("#nf-field-740")
-	# name_field.click()
-	# name_field.send_keys(user_name)
+# refactored to ninja forms class
+# @exception
+# def type_name():
+# 	# #find by css_selector --- this works for this page
+# 	# name_field = browser.find_element_by_css_selector("#nf-field-740")
+# 	# name_field.click()
+# 	# name_field.send_keys(user_name)
 
-	#find
-	name_field = browser.find_element_by_name("fname")
-	name_field.click()
-	name_field.send_keys(user_name)
+# 	#find
+# 	name_field = browser.find_element_by_name("fname")
+# 	name_field.click()
+# 	name_field.send_keys(user_name)
 
-@exception
-def type_phone():
-	#find by css_selector --- this works for this page	
-	# phone_field = browser.find_element_by_css_selector("#nf-field-741")
-	# phone_field.click()
-	# phone_field.send_keys(user_phone)
+# refactored to ninja forms class
+# @exception
+# def type_phone():
+# 	#find by css_selector --- this works for this page	
+# 	# phone_field = browser.find_element_by_css_selector("#nf-field-741")
+# 	# phone_field.click()
+# 	# phone_field.send_keys(user_phone)
 
-	phone_field = browser.find_element_by_name("phone")
-	phone_field.click()
-	phone_field.send_keys(user_phone)
+# 	phone_field = browser.find_element_by_name("phone")
+# 	phone_field.click()
+# 	phone_field.send_keys(user_phone)
 
-@exception
-def type_email():
-	#find by css_selector --- this works for this page	
-	# email_field = browser.find_element_by_css_selector("#nf-field-742")
-	# email_field.click()
-	# email_field.send_keys(user_email)
+# refactored to ninja forms class
+# @exception
+# def type_email():
+# 	#find by css_selector --- this works for this page	
+# 	# email_field = browser.find_element_by_css_selector("#nf-field-742")
+# 	# email_field.click()
+# 	# email_field.send_keys(user_email)
 
-	email_field = browser.find_element_by_name("email")
-	email_field.click()
-	email_field.send_keys(user_email)	
+# 	email_field = browser.find_element_by_name("email")
+# 	email_field.click()
+# 	email_field.send_keys(user_email)	
 
-@exception
-def next_page():
-	next_button = browser.find_element_by_class_name("nf-next")
-	next_button.click()
+# refactored to ninja forms class
+# @exception
+# def next_page():
+# 	next_button = browser.find_element_by_class_name("nf-next")
+# 	next_button.click()
 
 def fill_out_page_1():
 	type_name()
---
+++ Forms/ninja_forms_automator.py
@@ -0,0 +1,121 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
+
+import chromedriver_binary  # Adds chromedriver binary to path
+import functools #for error handling
+
+from time import sleep #to pause execution
+
+browser = ""
+user_name, email, phone = "", "", ""
+
+class Ninja_Forms_Automator():
+	"""
+	A Class for automatic generic ninja forms information
+	"""
+	def __init__():
+		pass
+
+	def exception(fn):
+	"""
+	A decorator that wraps the passed in function and logs exceptions should one occur
+	"""
+	@functools.wraps(fn)
+	def wrapper(*args, **kwargs):
+		try:
+			return fn(*args, **kwargs)
+		except NoSuchElementException:
+			print("There was a NoSuchElementException in " + fn.__name__) 
+
+	return wrapper
+
+	def get_default_user_values():
+	global user_name, user_email, user_phone
+	print("Proceeding with default values")
+
+	user_name = "Jeremy Marino"
+	user_email = "Jeremy@ccbf.net"
+	user_phone = "951-764-2881"
+
+	def get_user_info():
+	global user_name, user_email, user_phone
+
+	default_values = input("Do you want to use default values? 1 for yes : ")
+	if default_values == "1":
+		get_default_user_values()
+	else: 
+		user_name = input("Who's filling out the form? ")
+		user_email = input("What's the email? ")
+		user_phone = input("What's your phone number? ")
+
+	@exception
+	def get_forms_page():
+		global browser
+
+		browser.get("https://ccbf.net/wp-login.php?loggedout=true")#make sure you are logged out of ccbf
+		browser.get("https://ccbf.net/administrative-forms/") #get the forms login page
+
+		#get the password text box
+		password_element = browser.find_element_by_css_selector("#pwbox-1637") #get the password box
+		password_element.click()
+		password_element.send_keys("p@ssw0rd")
+
+		#click enter to enter password
+		submit_element = browser.find_element_by_name("Submit") #get the password box
+		submit_element.click()
+		# pause()
+		return browser
+
+	@exception
+	def switch_tabs():
+		# switch to the tab that opened the form
+		browser.find_element_by_tag_name('body').send_keys(Keys.CONTROL + Keys.TAB)
+		# print(len(browser.window_handles)) #check how many windows we have open, should be 2
+		browser.switch_to.window(browser.window_handles[-1])
+
+	@exception
+	def type_name():
+		# #find by css_selector --- this works for this page
+		# name_field = browser.find_element_by_css_selector("#nf-field-740")
+		# name_field.click()
+		# name_field.send_keys(user_name)
+
+		#find
+		name_field = browser.find_element_by_name("fname")
+		name_field.click()
+		name_field.send_keys(user_name)
+
+	@exception
+	def next_page():
+		next_button = browser.find_element_by_class_name("nf-next")
+		next_button.click()
+
+	@exception
+	def type_phone():
+		#find by css_selector --- this works for this page	
+		# phone_field = browser.find_element_by_css_selector("#nf-field-741")
+		# phone_field.click()
+		# phone_field.send_keys(user_phone)
+
+		phone_field = browser.find_element_by_name("phone")
+		phone_field.click()
+		phone_field.send_keys(user_phone)
+
+	@exception
+	def type_email():
+		#find by css_selector --- this works for this page	
+		# email_field = browser.find_element_by_css_selector("#nf-field-742")
+		# email_field.click()
+		# email_field.send_keys(user_email)
+
+		email_field = browser.find_element_by_name("email")
+		email_field.click()
+		email_field.send_keys(user_email)	
+
+
+
+
+
+
+
